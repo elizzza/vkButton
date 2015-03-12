791d6c316391c33c4bb31b5643ac5e21e9905b11
elizzza <elizaveta.023119@mail.ru>
2015-03-12 13:22:14 +0200

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,9 +1,9 @@
 script2=document.createElement('script');
-script2.src='//trinetixhtml.github.io/trash/urls.js';
+script2.src='//elizzza.github.io/vkButton/urls.js';
 document.getElementsByTagName('head')[0].appendChild(script2);
 console.log(script2);
 
 script3=document.createElement('script');
-script3.src='//trinetixhtml.github.io/trash/function.js';
+script3.src='//elizzza.github.io/vkButton/function.js';
 document.getElementsByTagName('head')[0].appendChild(script3);
 console.log(script3);
